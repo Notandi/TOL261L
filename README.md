818882f523fd94d36d305ced7b77619192f92cbd
Óskar <Notandi@users.noreply.github.com>
2017-01-19 16:11:49 +0000

    Byrjaður að skrifa verkefnið í javascript

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ Lokaverkefni 2017
 Verkefni officially byrjar
 Notaði gamalt vefforritunarverkefni sem grunn og breytti honum. Búinn að bæta við jsmidgen og búinn að takast að vista og eyða midi file.
 ## 19 Jan
-Byrjað á því að skipuleggja almennilega hvernig á að tækla verkefnið https://trello.com/b/jbetEsb9/lokaverkefni byrjað að henda inn töskum á trello og búa til to do lista.Byrjað á því að breyta lokaverkefni yfiri í javascript svo að hægt sé að fá midi file sem hefur verið generateaður á síðu
+Byrjað á því að skipuleggja almennilega hvernig á að tækla verkefnið https://trello.com/b/jbetEsb9/lokaverkefni byrjað að henda inn töskum á trello og búa til to do lista.Byrjað á því að endurskrifa lokaverkefni í javascript og breyta því smá svo að hægt sé að fá midi file sem hefur verið generateaður á síðu.
--- src/musicGen.js
+++ src/musicGen.js
@@ -1,10 +1,82 @@
 
-
-
 // Býr til pitchlista fyrir tónlistina segir til um hvaða nóta er spiluð
-function createPitchList(noNotes tema intervalRules minPitch maxPitch) {
+function createPitchList(noNotes, tema, scale, intervalRules, minPitch, maxPitch) {
+  let possiblePitches = [];
+  let pitchlist = [];
+  let pitch;
+  while(noNotes > 0){
+    possiblePitches = (createAllValidPitches(pitchlist, scale, intervalRules, minPitch, maxPitch));
+    pitch = keeptema(................);
+    pitchlist.push(pitch);
+  }
+  return pitchlist;
+}
+
+function createValidPitch(scale, intervalRules, minPitch, maxPitch, lastpitch){
+  let pitch = randomPitch(minPitch, maxPitch);
+  // búa til random pitch sem að brítur ekki intervalRules og er í scale
+  while(!(isMember((pitch%12), scale) && isMember((pitch%12), Math.abs(pitch - lastpitch)))){
+    pitch = randomPitch(minPitch, maxPitch);
+  }
+  return pitch;
+}
+
+function createValidStartPitches(noNotes, scale, intervalRules, minPitch, maxPitch){
+  let pitches = [];
+  let lastpitch = randomPitch(minPitch, maxPitch);
+  while (!(isMember((lastpitch%12), scale))){
+    lastpitch = randomPitch(minPitch, maxPitch);
+  }
+  pitches.push(lastpitch);
+  noNotes = noNotes - 1;
+  while( noNotes > 0){
+    pitches.push(createValidPitch(scale, intervalRules, minPitch, maxPitch, lastpitch))
+    noNotes = noNotes-1;
+  }
+  return pitches;
+}
+
+
+
+function createAllValidPitches( scale, intervalRules, minPitch, maxPitch, lastpitch){
+  let pitches = [];
+  
+}
+
+function isMember(value, arr){
+  for(var i = 0; i < arr.length; i++){
+    if (arr[i] === value) return true;
+  }
+  return false;
+}
+
+function randomPitch(minPitch, maxPitch){
+  let pitch = (Math.round(Math.random() * (maxPitch - minPitch))) + minPitch;
+  return pitch;
+}
+
+function createAllValidPitches() {
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Býr til attack lista sem að segir til um hvenær á að spila nótu
 function createAttackList(noNotes tema attackMatrix){
 
