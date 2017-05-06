a67179df1c5ca1869ec491ad42170085dbda3af4
Óskar Ólafsson <Notandi@users.noreply.github.com>
2017-05-06 15:21:44 +0000

    python scripta sem að býr til markovkeðju er að fara að verða til

--- pythonscripts/script.py
+++ pythonscripts/script.py
@@ -1,12 +1,46 @@
-# script.py
-my_name = 'Carlos'
-my_age = 18 # not a lie
-my_height = 172 # cm
-my_weight = 71 # kg
-my_eyes = 'Brown'
-my_teeth = 'White'
-my_hair = 'Black'
+import numpy as np
 
-# this line is tricky, try to get it exactly right
-print ("If I add %d, %d, and %d I get %d. I don't know what that means but, whatever." % (
-    my_age, my_height, my_weight, my_age + my_height + my_weight))
+distribution = np.array([0.35,0.13,0.13,0.065,0.065,0.13,0.13])
+modifierDistribution = np.array([0.025,0.05,0.1,0.15,0.35,0.15,0.1,0.05,0.025])
+
+# býr til markovkeðjuna með því að gera matrixu
+def createNewMarkovChain (scale):
+    markovSize = 108
+    modifier = createMarkovScaleModifier(scale,markovSize)
+
+    if isModOf(0,scale):
+        markovChain = np.array([modifier])
+    else:
+        markovChain = np.array([np.zeros(markovSize)])
+
+    for num in range(1,markovSize):
+        if isModOf(num,scale):
+            markovChain = np.concatenate((markovChain, np.array([modifier])), axis= 0)
+        else:
+            markovChain = np.concatenate((markovChain, np.array([np.zeros(markovSize)])), axis= 0)
+    return markovChain
+
+def createMarkovScaleModifier (scale, length):
+    scaleModifier = np.array([])
+    for num in range(0,9):
+        octaveChain = generateScaleDistrubution(scale, modifierDistribution[num])
+        scaleModifier = np.concatenate((scaleModifier,octaveChain), axis = 0)
+    return scaleModifier
+
+def generateScaleDistrubution (scale, modifier):
+    octaveSteps = 12
+    octaveArray = np.zeros(octaveSteps)
+    for num in range(0,scale.size):
+        pitch = scale[num]
+        chance = distribution[num] * modifier
+        octaveArray[pitch] = chance
+    return octaveArray
+
+
+def isModOf (pitch, scale):
+    for num in range(0, scale.size):
+        if (pitch%12 == scale[num]):
+             return True
+    return False
+
+mchain = createNewMarkovChain(np.array([8,10,11,1,3,4,6]));
